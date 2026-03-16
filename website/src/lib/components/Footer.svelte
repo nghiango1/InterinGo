a4558d9b22dd08e1f74394297138a3513fdb7db7
nghiango1 <ducnghia.tin47@gmail.com>
2026-03-16 21:53:23 +0700

    feat: Svelte cover Homepage first component

---
+++ website/src/lib/components/Footer.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import ShortInfo from '$lib/components/ShortInfo.svelte';
+	
+</script>
+<footer
+	class="prose flex max-w-none flex-col gap-4 border-t-2 border-t-blue-200 pt-2 sm:flex-row-reverse sm:pt-8 dark:prose-invert"
+>
+	<div class="flex flex-col sm:ml-auto sm:text-right">
+		<h2 class="font-bold">Menu</h2>
+		<a class="text-blue-500 underline" href="/">home</a>
+		<a class="text-blue-500 underline" href="/info">about</a>
+		<a class="text-blue-500 underline" href="/404">404</a>
+	</div>
+
+	<ShortInfo />
+</footer>
--- website/src/routes/+page.svelte
+++ website/src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang='ts'>
+	import Home from '$lib/pages/Home.svelte';
+</script>
+
+<Home />
