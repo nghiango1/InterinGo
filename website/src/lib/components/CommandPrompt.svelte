6a93ee6106957ed904e9437185a8bc905f1a72b1
nghiango1 <ducnghia.tin47@gmail.com>
2026-03-17 11:34:11 +0700

    feat: reactive state for Command Prompt

--- website/src/lib/components/CommandPrompt.svelte
+++ website/src/lib/components/CommandPrompt.svelte
@@ -1,14 +1,76 @@
 <script lang="ts">
+	import Line from './command_prompt/Line.svelte';
+	import { postEvaluate } from '$lib/controller/repl';
+	import { type EvalRequest, type EvalResponseSuccess } from '$lib/server/repl';
+	import { onMount, tick } from 'svelte';
+
+	let isEval = $state(false);
+	let stick = $state(false);
+	let hide = $state(false);
+	let wrap = $state(false);
+
+	const STARTED_LINE = 'Let start with help() command';
+	let lines: string[] = $state([STARTED_LINE]);
+	let command = $state('');
+
+	function updateStick() {
+		stick != stick;
+	}
+	function updateWrap() {
+		wrap != wrap;
+	}
+
+	// Bind to the command prompt output element
+	// svelte-ignore non_reactive_update
+	let replOutput: HTMLElement;
+
+	// Which we have to use effect until I found better solution
+	$effect(() => {
+		// Access lines.length to create a reactive dependency
+		if (lines.length && replOutput) {
+			replOutput.scrollTop = replOutput.scrollHeight;
+		}
+	});
+
+	function addCommand() {
+		lines.push(`>> ${command}`);
+	}
+
+	function copyEvalResult(resp: EvalResponseSuccess) {
+		lines.push(resp.output);
+	}
+
+	async function evaluate() {
+		isEval = true;
+		const req: EvalRequest = {
+			data: command
+		};
+		addCommand();
+
+		const resp = await postEvaluate(req);
+
+		if (resp.status == 200) {
+			copyEvalResult(resp);
+			// command = '';
+		}
+		isEval = false;
+	}
 </script>
 
-<input type="checkbox" id="hiddenstick" class="peer/hiddenstick hidden" checked="true" />
-<div class="block peer-checked/hiddenstick:sticky peer-checked/hiddenstick:top-4 xl:w-5/12">
+<input type="checkbox" id="hiddenstick" class="peer/hiddenstick hidden" bind:checked={stick} />
+<div class={(stick ? 'sticky' : 'top-4') + 'block xl:w-5/12'}>
 	<div
 		class="sticky top-4 flex flex-col gap-1 before:absolute before:-inset-4 before:-top-4 before:-z-10 before:rounded-b-lg before:bg-white/30 before:object-none before:backdrop-blur-sm after:absolute after:-inset-2 after:-top-2 after:-z-10 after:rounded-b-lg after:bg-white/30 after:object-none after:blur-sm dark:text-[#d1d5db] before:dark:bg-[#050510]/30 after:dark:bg-[#050510]/30"
 	>
 		<div class="overflow-hidden rounded-t-lg border-2 border-blue-500 dark:border-white">
 			<input id="hiddenwrap" class="peer/wrap hidden" type="checkbox" name="wrap" />
-			<input id="hiddenhide" class="peer/hide hidden" type="checkbox" name="hide" />
+			<input
+				id="hiddenhide"
+				class="peer/hide hidden"
+				type="checkbox"
+				bind:checked={hide}
+				name="hide"
+			/>
 			<div class="flex gap-2 border-b-2 bg-blue-200 p-1 dark:bg-[#090d1a]">
 				<h2 class="m-auto block flex-1 overflow-clip whitespace-nowrap">Command-prompt window</h2>
 				<input
@@ -16,7 +78,7 @@
 					class="peer/wrap m-auto"
 					type="checkbox"
 					name="wrap"
-					oninput={'updateWrap(); scrollBottom();'}
+					oninput={updateWrap}
 				/>
 				<label
 					for="wrap"
@@ -28,9 +90,9 @@
 					id="stick"
 					class="peer/stick m-auto"
 					type="checkbox"
-					name="hide"
-					oninput={'updateStick();'}
-					checked="true"
+					name="stick"
+					oninput={updateStick}
+					checked={true}
 				/>
 				<label
 					for="stick"
@@ -38,13 +100,7 @@
 				>
 					Stick
 				</label>
-				<input
-					id="hide"
-					class="peer/hide m-auto"
-					type="checkbox"
-					name="hide"
-					oninput={'updateHide(); scrollBottom();'}
-				/>
+				<input id="hide" class="m-auto" type="checkbox" name="hide" bind:checked={hide} />
 				<label
 					for="hide"
 					class="m-auto flex flex-row gap-2 rounded-lg peer-checked:bg-gray-200 peer-checked/hide:font-bold"
@@ -52,24 +108,19 @@
 					Hide
 				</label>
 			</div>
-			<pre
-				id="repl-output"
-				class="scrollbar flex h-56 resize-y overflow-auto rounded-lg p-2 whitespace-pre outline-blue-200 peer-checked/hide:hidden peer-checked/wrap:whitespace-pre-wrap"
-				hx-on:htmx:after-swap="scrollBottom()"> Let start with help() command {'\n'} </pre>
-			<pre
-				id="repl-result"
-				class="hidden max-h-6 overflow-auto px-2 peer-checked/hide:block"
-				hx-on:htmx:after-swap="copyEvalResult();scrollBottom();"></pre>
+			{#if hide}
+				<pre id="repl-result" class="block max-h-6 overflow-auto px-2"></pre>
+			{:else}
+				<pre
+					id="repl-output"
+					bind:this={replOutput}
+					class="scrollbar flex h-56 resize-y overflow-auto rounded-lg p-2 whitespace-pre outline-blue-200 peer-checked/wrap:whitespace-pre-wrap">{#each lines as line}<Line
+							{line}
+						/>{/each}</pre>
+			{/if}
 		</div>
-		<form
-			class="flex h-fit flex-row gap-4 outline-blue-200 dark:outline-white"
-			hx-post="/api/evaluate"
-			hx-target="#repl-result"
-			hx-swap="innerHTML"
-			hx-on:htmx:config-request="addCommand()"
-			hx-ext="json-enc"
-		>
-			<p class="hidden sm:my-auto sm:block">Custom command:</p>
+		<form class="flex h-fit flex-row gap-4 outline-blue-200 dark:outline-white" method="POST">
+			<label for="repl-input" class="hidden sm:my-auto sm:block">Custom command:</label>
 			<input
 				class="my-auto flex-1 border-b border-gray-500 bg-transparent pl-2 font-mono focus:outline-none"
 				type="text"
@@ -83,12 +134,14 @@
 				enterkeyhint="go"
 				spellcheck="false"
 				placeholder="help()"
+				bind:value={command}
 			/>
 			<button
 				class="activate:dark:bg-blue-900 my-auto block rounded-lg border-2 bg-blue-200 p-1 active:bg-blue-200 dark:bg-[#090d1a]"
 				id="repl-send"
-				type="Summit"
-				click="addCommand()"
+				type="submit"
+				disabled={isEval}
+				onclick={evaluate}
 			>
 				Run
 			</button>
