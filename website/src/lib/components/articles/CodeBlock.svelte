663ad22e5bcc0730936b9df44f838d00aeb722cc
nghiango1 <ducnghia.tin47@gmail.com>
2026-03-17 21:44:03 +0700

    feat: Main article

---
+++ website/src/lib/components/articles/CodeBlock.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	let {
+		name,
+		code,
+		setCommand
+	}: {
+		name: string;
+		code: string;
+		setCommand: (input: string) => void;
+	} = $props();
+
+	function handleClick() {
+		setCommand(code);
+	}
+</script>
+
+<div class="flex flex-col">
+	<div class="flex flex-row">
+		<p class="my-auto block flex-1">Snippet - <code>{name}</code></p>
+		<button class="my-auto block rounded-lg underline" {name} onclick={handleClick}>
+			Copy and Run
+		</button>
+	</div>
+	<pre id={name}>{code}</pre>
+</div>
