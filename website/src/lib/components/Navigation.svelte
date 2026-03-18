0d859a6320bae985a8ffa0bd22092f8cbb36e4fd
nghiango1 <ducnghia.tin47@gmail.com>
2026-03-18 18:16:05 +0700

    feat: Tailwind covered, Navigation on Doc page should be ok

--- website/src/lib/components/Navigation.svelte
+++ website/src/lib/components/Navigation.svelte
@@ -20,13 +20,15 @@
 	}
 </script>
 
-{#if !hide}
-	<button
-		class={'sticky top-16 block rounded-r-lg bg-gray-200 p-1 [writing-mode:vertical-lr] focus:bg-blue-500 focus:font-bold active:bg-blue-500 md:hidden dark:bg-gray-500 dark:text-white'}
-		onclick={handleHide}
-	>
-		Show menu
-	</button>
-{:else}
+<input id="linked" class="peer m-auto hidden" type="checkbox" name="checked" bind:checked={hide} />
+<label
+	for="linked"
+	class="sticky top-16 block rounded-r-lg bg-gray-200 p-1 [writing-mode:vertical-lr] peer-checked:hidden focus:bg-blue-500 focus:font-bold active:bg-blue-500 md:hidden dark:bg-gray-500 dark:text-white"
+>
+	Show menu
+</label>
+<div
+	class="sticky top-4 hidden h-full w-[19rem] px-4 peer-checked:block before:absolute before:-inset-0 before:-top-4 before:-z-10 before:rounded-b-lg before:bg-white/30 before:object-none before:backdrop-blur-lg md:block dark:text-white before:dark:bg-[#050510]/30"
+>
 	<NavigationSession {docs} {name} {handleHide} />
-{/if}
+</div>
