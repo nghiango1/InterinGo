bc3894a2c1856bb1dc20b6191d46a5d1ab95e297
nghiango1 <ducnghia.tin47@gmail.com>
2026-04-12 16:48:33 +0700

    feat: now vite will route the api to the backend for development purpose
    - WS is shard to implement into the dev node application, this is just better
    - API in other hand need to be sync between client and server. JSON scheme
    will need to be added later

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,6 +1,6 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
 import { compile } from 'mdsvex';
 import { readFileSync } from 'fs';
@@ -42,17 +42,28 @@ async function getDocs(): Promise<DocInfo[]> {
 	return docs;
 }
 
-export default defineConfig({
-	server: {
-		port: 3000,
-		open: true,
-		host: true
-	},
-	plugins: [tailwindcss(), sveltekit()],
-	define: {
-		// Double JSON.stringify:
-		//   outer → valid JS string literal for Vite's define substitution
-		//   inner → the JSON payload that JSON.parse() reads at runtime
-		'import.meta.env.VITE_DOCS': JSON.stringify(JSON.stringify(await getDocs()))
+export default defineConfig(async ({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '')
+
+	return {
+		server: {
+			port: 3000,
+			open: true,
+			host: true,
+			proxy: {
+				'/ws': {
+					target: env.BACKEND_SERVER_URL,
+					ws: true,
+					rewriteWsOrigin: true,
+				}
+			}
+		},
+		plugins: [tailwindcss(), sveltekit()],
+		define: {
+			// Double JSON.stringify:
+			//   outer → valid JS string literal for Vite's define substitution
+			//   inner → the JSON payload that JSON.parse() reads at runtime
+			'import.meta.env.VITE_DOCS': JSON.stringify(JSON.stringify(await getDocs()))
+		}
 	}
 });
