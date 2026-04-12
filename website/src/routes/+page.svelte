e7c690858a7aabce0b4d2ee4b08f7290e7fe6f1e
nghiango1 <ducnghia.tin47@gmail.com>
2026-04-12 22:42:24 +0700

    fix: only main page have different font

--- website/src/routes/+page.svelte
+++ website/src/routes/+page.svelte
@@ -1,5 +1,12 @@
-<script lang='ts'>
+<script lang="ts">
 	import Home from '$lib/pages/Home.svelte';
 </script>
+
+<svelte:head>
+	<link
+		href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500;700&family=Space+Grotesk:wght@400;500;700&display=swap"
+		rel="stylesheet"
+	/>
+</svelte:head>
 
 <Home />
--- website/src/routes/docs/+layout.svelte
+++ website/src/routes/docs/+layout.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <div class="grid px-8 md:grid-cols-(--docs-grid-cols)">
-	<div class="fixed top-0 bottom-0 left-0 z-20 min-w-[2rem] overflow-y-auto md:static">
+	<div class="fixed top-0 bottom-0 left-0 z-20 min-w-8 overflow-y-auto md:static">
 		<Navigation nav={data.navSessions}/>
 	</div>
 	<div class="overflow-auto">
