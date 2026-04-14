93dc640ac0bf8f9c9ecfb44b27845ce25010183d
nghiango1 <ducnghia.tin47@gmail.com>
2026-04-14 21:33:35 +0700

    fix: ws may not understand relative url, cause error 500

--- website/src/routes/+layout.ts
+++ website/src/routes/+layout.ts
@@ -7,3 +7,18 @@ export const ssr = false;
 // - No directory 'docs.html' is used as literal file, golang doesn't support
 // route it to /docs
 export const trailingSlash = 'always';
+
+// Keep it here for later
+import type { LayoutLoad } from "./$types";
+import { commandPromptState, WebSocketImpl } from "$lib/components/CommandPromptState.svelte";
+
+export function load({ }: LayoutLoad) {
+	console.log("[INFO] Create new WS connection to the server")
+	// init commandPromptState new websocket
+	commandPromptState.ws = new WebSocketImpl()
+
+	return {
+		init: true,
+	}
+}
+
