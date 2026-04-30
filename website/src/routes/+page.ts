7720f8ecc93dcbebab57c62bccf7b32999f13bd0
nghiango1 <ducnghia.tin47@gmail.com>
2026-04-30 15:37:37 +0700

    fix: reduce the ws reconnect every time navigate between page

--- website/src/routes/+page.ts
+++ website/src/routes/+page.ts
@@ -1,10 +1,18 @@
 // Keep it here for later
+import {
+	commandPromptState,
+	WebSocketImpl,
+} from "$lib/components/CommandPromptState.svelte";
 import type { PageLoad } from "./$types";
 
-export function load({} : PageLoad) {
-	console.log("[INFO] Loaded page done")
+export function load({ }: PageLoad) {
+	// init commandPromptState new websocket
+	if (commandPromptState.ws == null) {
+		console.log("[INFO] Create new WS connection to the server");
+		commandPromptState.ws = new WebSocketImpl();
+	}
+
 	return {
 		init: true,
-	}
+	};
 }
-
